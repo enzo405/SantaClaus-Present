39ea0ebe07ef3a88e7aaa86e59b71f53e18e56fa
Enzo <75796536+enzo405@users.noreply.github.com>
2022-12-05 10:01:21 +0100

    always return result fix

--- SantaClaus.py
+++ SantaClaus.py
@@ -1,23 +1,24 @@
 import random
 import re
+import traceback
 
-## nombre x de personne
+# nombre x de personne
 try:
 	nbr_personne = int(input("Choisir le nombre de personne : "))
-except: 
+except:
 	print('You must enter a number')
 	nbr_personne = "1"
-while type(nbr_personne) != int:
+while type(nbr_personne) != int or nbr_personne < 2:
 	try:
 		nbr_personne = int(input("Choisir le nombre de personne : "))
 	except: 
 		print('You must enter a number')
 
-
 ## définir le nom de toute les personnes
-dictPerson = {}
-allName = []
-contrainteDict = {}
+dictPerson = {0: 'enzo', 1: 'raphael', 2: 'sam', 3: 'rossella'}
+dictEmail = {0: 'enzo.chaboisseau@gmail.com', 1: 'raphael.chaboisseau@gmail.com', 2:'sam@chaboisseau.net', 3:'none'}
+allName = ['enzo','raphael','sam', 'rossella']
+contrainteDict = {'enzo': ['raphael'], 'raphael': ['enzo', 'sam'], 'sam': ['rossella'], 'rossella': ['sam']}
 
 for i in range(nbr_personne):
 	name = input(f"Rentrez le nom n°{i+1}: ")
@@ -29,12 +30,13 @@ for i in range(nbr_personne):
 		if email_pattern.match(email):
 			break
 		elif email == "no":
+			email = "none"
 			break
 		else:
 			email = input(f"Rentrez l'adresse E-mail de {name}: ")
-	dictPerson.update({i:f"{name} {email}"})
+	dictPerson.update({i:name})
+	dictEmail.update({i:email})
 	allName.append(name)
-print(dictPerson)
 
 
 ## définir les contraintes qu'aura cette personne
@@ -49,33 +51,35 @@ for i in range(nbr_personne):
 				nameContrainte = input(f"RE Enter a list (separate with ',') that {dictPerson[i]} won't be abble to send: ").replace(' ','').split(",")
 	while dictPerson[i] in nameContrainte:
 		nameContrainte = input(f"Enter a list (separate with ',') that {dictPerson[i]} won't be abble to send: ").replace(' ','').split(",")
-	contrainteDict.update({dictPerson[i]:nameContrainte})
+	contrainteDict.update({dictPerson[i][0]:nameContrainte})
 
 
 ## relier les personnes entre elles pour connaitre son SantaClaus
-def ChooseSantaClaus(dictPerson:dict,allName:list,contrainteDict:dict):
-	alreadyUsedPerson = ""
+def ChooseSantaClaus(fDictPerson:dict,fAllName:list,fContrainteDict:dict):
+	global dictPerson
+	global allName
+	global contrainteDict
 	kdo = {}
-	for i in allName:
-		availableName = list(dictPerson.keys())
-		try:
-			availableName.remove(alreadyUsedPerson)
-		except ValueError as ee:
-			print(ee,availableName,alreadyUsedPerson)
-		for x in availableName:
-			if dictPerson[x] == i:
-				availableName.remove(x)
-		for y in contrainteDict[i]:
-			value = list(dictPerson.values())
+	global_availableName = list(fDictPerson.keys())
+	for i in fAllName:
+		selfAvailableName = global_availableName.copy()
+		for x in selfAvailableName:
+			if fDictPerson[x] == i:
+				selfAvailableName.remove(x)
+		for y in fContrainteDict[i]:
+			value = list(fDictPerson.values())
 			indexPersonContraint = ''
 			try:
 				indexPersonContraint = value.index(y)
-				availableName.remove(indexPersonContraint)
+				selfAvailableName.remove(indexPersonContraint)
 			except ValueError as e:
-				print(e,availableName,indexPersonContraint)
-		toName = random.choice(availableName)
-		alreadyUsedPerson = toName
-		kdo.update({i:dictPerson[toName]})
+				print(f"{selfAvailableName}.remove({indexPersonContraint})")
+		if len(selfAvailableName) == 0:
+			ChooseSantaClaus(dictPerson, allName, contrainteDict)
+		else:
+			toName = random.choice(selfAvailableName)
+			global_availableName.remove(toName)
+			kdo.update({i:fDictPerson[toName]})
 	return kdo
 
 
