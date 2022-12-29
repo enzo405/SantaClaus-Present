165dd6e5cf0ca5feed953cd7917e0c0cdb5e7f25
Enzo <75796536+enzo405@users.noreply.github.com>
2022-12-29 13:17:44 +0100

    range for number **0.5

--- SantaClaus.py
+++ SantaClaus.py
@@ -5,10 +5,11 @@ from name import prenom
 
 
 ## relier les personnes entre elles pour connaitre son SantaClaus
-def ChooseSantaClaus(selfDictPerson:dict,selfAllName:list,selfContrainteDict:dict, selfnbrtry:int, choix:bool):
+def ChooseSantaClaus(selfDictPerson:dict,selfAllName:list,selfContrainteDict:dict, selfnbrtry:int):
 	global dictPerson
 	global allName
 	global contrainteDict
+	global choix_version
 	kdo = {}
 	global_availableName = list(selfDictPerson.keys())
 	for i in selfAllName:
@@ -30,10 +31,9 @@ def ChooseSantaClaus(selfDictPerson:dict,selfAllName:list,selfContrainteDict:dic
 			kdo.update({i:selfDictPerson[toName]})
 		elif len(selfAvailableName) == 0 and selfnbrtry < 10:
 			selfnbrtry = selfnbrtry + 1
-			ChooseSantaClaus(dictPerson, allName, contrainteDict,selfnbrtry)
-			break
+			ChooseSantaClaus(dictPerson, allName, contrainteDict, selfnbrtry)
 		elif selfnbrtry >= 10:
-			if choix == True:
+			if choix_version == True:
 				os.system('cls')
 				print(f"Vos choix ne sont pas possible, veuillez changer la contrainte de {i} :")
 				print(allName)
@@ -48,8 +48,8 @@ def ChooseSantaClaus(selfDictPerson:dict,selfAllName:list,selfContrainteDict:dic
 				while i in nameContrainte or len(nameContrainte) == len(allName)-1 :
 					nameContrainte = input(f"Enter a list (separate with ',') that {i} won't be abble to send: ").replace(' ','').split(",")
 				contrainteDict.update({i:nameContrainte})
-				ChooseSantaClaus(dictPerson, allName, contrainteDict,0, choix=True)
-			elif choix == False:
+				ChooseSantaClaus(dictPerson, allName, contrainteDict,0)
+			elif choix_version == False:
 				for o in range(len(allName)):
 					rangefor = random.randint(0,len(allName)%2)
 					for k in range(rangefor):
@@ -57,14 +57,13 @@ def ChooseSantaClaus(selfDictPerson:dict,selfAllName:list,selfContrainteDict:dic
 						while random_number2 == random_number:
 							random_number2 = random.randint(0,len(allName)-1)
 						icontrainte.append(allName[random_number2])
-					contrainteDict.update({allName[k]:icontrainte})
-				ChooseSantaClaus(dictPerson, allName, contrainteDict, 0, choix=False)
+					contrainteDict.update({allName[o]:icontrainte})
+				ChooseSantaClaus(dictPerson, allName, contrainteDict, 0)
 	if len(kdo) != len(allName):
 		for i in allName:
 			if i not in kdo:
 				kdo.update({i:'personne'})
 	return kdo
-
 
 
 choix = int(input("Vous préférez :\n\t1 - Version anonymes\n\t2 - Versions avec vos propres nom, e-mail, contraintes ...etc\n"))
@@ -74,22 +73,25 @@ if choix == 1:
 	dictEmail = {}
 	allName = []
 	contrainteDict = {}
-	icontrainte = []
 	for i in range(nbr_personne):
 		random_number = random.randint(1,len(prenom))
 		dictPerson.update({i:prenom[random_number]})
 		dictEmail.update({prenom[random_number]:f"{prenom[random_number]}@gmail.com"})
 		allName.append(prenom[random_number])
 	for j in range(nbr_personne):
-		rangefor = random.randint(0,len(allName)%2)
+		endrangefor = int(len(allName)**0.5)
+		rangefor = random.randint(0,endrangefor)
+		print(rangefor)
+		icontrainte = []
 		for x in range(rangefor):
 			random_number2 = random.randint(0,len(allName)-1)
 			while random_number2 == random_number:
 				random_number2 = random.randint(0,len(allName)-1)
 			icontrainte.append(allName[random_number2])
 		contrainteDict.update({allName[j]:icontrainte})
-
-	kdo = ChooseSantaClaus(dictPerson, allName, contrainteDict,0, choix=False)
+	choix_version = False
+	kdo = ChooseSantaClaus(dictPerson, allName, contrainteDict,0)
+	
 else:
 	#nombre x de personne
 	try:
@@ -108,6 +110,7 @@ else:
 	dictEmail = {}
 	allName = []
 	contrainteDict = {}
+	choix_version = True
 
 	for i in range(nbr_personne):
 		name = input(f"Rentrez le nom n°{i+1}: ")
@@ -138,8 +141,16 @@ else:
 			nameContrainte = input(f"Enter a list (separate with ',') that {dictPerson[i]} won't be abble to send: ").replace(' ','').split(",")
 		contrainteDict.update({dictPerson[i]:nameContrainte})
 
-	kdo = ChooseSantaClaus(dictPerson, allName, contrainteDict,0, choix=True)
+	kdo = ChooseSantaClaus(dictPerson, allName, contrainteDict,0)
+
+
+try:
+	result_file = open('result-email.txt','x', encoding='utf-8')
+except:
+	result_file = open('result-email.txt','w', encoding='utf-8')
 
 for i in allName:
 	templateMail = f"Bonjour {i} 👋\n\tVoici le jeux SantaClaus, une personne de votre entourage à été tiré au hasard, vous allé devoir acheter un cadeau à {kdo[i]}"
-	print(dictEmail[i],templateMail)
+	result_file.write(f"{dictEmail[i]} {templateMail}\n\n")
+
+result_file.write(f"\nAll name: {allName}\nParticipant: {dictPerson}\nEmail Participant: {dictEmail}\nContrainte: {contrainteDict}\n")
